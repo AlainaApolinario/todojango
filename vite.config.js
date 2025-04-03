bc412c0084c9b473cc4242f456ea6ae49d7d7dfe
AlainaApolinario <apolinarioalainabacal@gmail.com>
2025-04-03 21:44:05 +0800

    okay na?

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Todo/", // Fix incorrect asset paths
+  base: "/todojango/", // ✅ Set this to match your repo name
 });
