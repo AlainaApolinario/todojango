2eb1e0fadb697dd236300aaa0324b23c8aaee3b5
AlainaApolinario <apolinarioalainabacal@gmail.com>
2025-04-03 21:52:54 +0800

    okay na?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/todojango/", // ✅ Set this to match your repo name
+  base: '/Todo/',  // 👈 This ensures correct asset paths
 });
